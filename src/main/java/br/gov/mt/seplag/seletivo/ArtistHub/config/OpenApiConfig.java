44354b872a9ccbc42f87f24d63151defc85afe4b
Jonathan Santana Silva <jonathan-works@hotmail.com>
2026-02-04 20:54:03 -0400

    fix: correção swagger com token

--- src/main/java/br/gov/mt/seplag/seletivo/ArtistHub/config/OpenApiConfig.java
+++ src/main/java/br/gov/mt/seplag/seletivo/ArtistHub/config/OpenApiConfig.java
@@ -3,8 +3,11 @@ package br.gov.mt.seplag.seletivo.ArtistHub.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
@@ -12,9 +15,17 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI artistHubOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
                 .info(new Info()
                         .title("ArtistHub API")
                         .description("API para gerenciamento de artistas e álbuns")
                         .version("v1"));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
